158e11621fa73632e3d2853d2f473b07312c5eed
simonky <simonasb19@gmail.com>
2025-06-27 08:33:44 -0400

    First commit

---
+++ src/test/java/com/rukayun/bff/BffApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rukayun.bff;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BffApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
